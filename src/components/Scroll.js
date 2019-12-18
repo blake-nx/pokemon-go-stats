e9e201a4291b7bfec3e528f5371c6b147cc6852b
blakewenloe <root@vps189071.vps.ovh.ca>
2019-12-18 00:38:39 -0500

    updated scroll height

--- src/components/Scroll.js
+++ src/components/Scroll.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Scroll = (props) => {
   return (
-    <div style={{ overflow: 'scroll', height: '800px'}}>
+    <div style={{ overflow: 'scroll'}}>
       {props.children}
     </div>
   );
--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -4,7 +4,7 @@ const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input
-        className='pa3 ba b--green w-33'
+        className='pa3 ba b--green w-50'
         type='search'
         placeholder='search pokemon'
         onChange={searchChange}
